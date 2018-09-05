6a1f77af4a346e711f483b7603382df6af08b7de
tom0935 <z00178lawbroker@gmail.com>
2018-09-05 21:01:14 +0800

    webapi

---
+++ Mvc_WebApi_Application/MvcApplication4/JwtAuthObject.cs
@@ -0,0 +1,9 @@
+namespace JwtAuthWebApi
+{
+    public class JwtAuthObject
+    {
+        public string Id { get; set; }
+        public double exp { get; set; }
+        public double iat { get; set; }
+    }
+}
